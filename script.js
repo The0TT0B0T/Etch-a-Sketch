1fc0c10bac984f39f3b653005fdb0b322872a03b
Otto Ayala <oayala543@gmail.com>
2024-09-18 18:26:58 -0500

    Created the 16x16 grid and everytime the mouse hovers over each div a random color will appear. Next I have to create a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid sholde be removed, and the new grid should be generated.

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
 const container = document.querySelector('.container');
 
-// creating 16x16 square divs
 const grid = () => {
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
-            const div =  document.createElement('div');
-
-            // Set the div's class to "grid-item" for styling
-            div.classList.add('grid-item');
-
-            // Append the div to the container
-            container.appendChild(div);
-        }
-        
+    for (let i = 0; i < 256; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = randomColor();
+        });
     }
-    const randomColor = () => {
-    const letters = '0123456789ABCDEF'
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-    }
-    div.style.backgroundColor = randomColor();
-}
+};
 
+const randomColor = () => {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+        color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+};
 
 grid();
